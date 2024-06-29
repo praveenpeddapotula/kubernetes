49471adbbae3f6d9ccbfbd48d678d4856dac31a4
praveenpeddapotula <praveen.peddapotula1@gmail.com>
2024-06-29 18:28:35 +0530

    added kubectl and eksctl installation file

---
+++ kubectl-eksctl-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###install eksctl ####
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+#####install kubectl #####
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+ sudo chmod +x kubectl
+sudo mv ./kubectl ~/.local/bin/kubectl
