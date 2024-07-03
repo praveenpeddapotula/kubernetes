a7fb4ea6b2c4bf957e968205a927c89f7cdaaba2
praveenpeddapotula <praveen.peddapotula1@gmail.com>
2024-07-03 19:15:40 +0530

    modified kubectl-eksctl-install.sh

--- kubectl-eksctl-install.sh
+++ kubectl-eksctl-install.sh
@@ -6,7 +6,9 @@ sudo mv /tmp/eksctl /usr/local/bin
 
 #####install kubectl #####
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod +x kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check  
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo chmod +x kubectl
 mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
